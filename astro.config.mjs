ad11af12891c30a8e44ffee8d41197712c7cbbf0
otobongfp <otobongfp@gmail.com>
2024-04-29 01:39:01 +0100

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  //   integrations: [
+  //     // Enable Preact to support Preact JSX components.
+  //     preact(),
+  //     // Enable React for the Algolia search component.
+  //     react(),
+  //     tailwind(),
+  //   ],
+  //site: `https://www.christophermarkus.com`,
+  output: "static",
+  outDir: "./docs",
+  build: {
+    assets: "astro",
+  },
+});
