763467cad191a8dfaa7e9e627551a5d9d630a385
otobongfp <otobongfp@gmail.com>
2024-05-12 00:53:34 +0100

    minor updates to content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,26 @@
-// import { defineConfig } from "astro/config";
-
-// import vercel from "@astrojs/vercel/serverless";
-
-// // https://astro.build/config
-// export default defineConfig({
-//   //   integrations: [
-//   //     // Enable Preact to support Preact JSX components.
-//   //     preact(),
-//   //     // Enable React for the Algolia search component.
-//   //     react(),
-//   //     tailwind(),
-//   //   ],
-//   site: `https://otobongfp.github.io/site/`,
-//   output: "server",
-//   outDir: "./docs",
-//   build: {
-//     assets: "astro",
-//   },
-//   adapter: vercel(),
-// });
-
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  //   integrations: [
+  //     // Enable Preact to support Preact JSX components.
+  //     preact(),
+  //     // Enable React for the Algolia search component.
+  //     react(),
+  //     tailwind(),
+  //   ],
+  site: `https://otobongfp.github.io/site/`,
+  output: "server",
+  outDir: "./docs",
+  build: {
+    assets: "astro",
+  },
+  adapter: vercel(),
+});
+
+// import { defineConfig } from "astro/config";
+
+// // https://astro.build/config
+// export default defineConfig({});
